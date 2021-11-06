8927bb165fffbecbc1b7848a4c4a461ad5a45de4
thcheetah777 <thcheetah777@gmail.com>
2021-11-06 15:05:59 -0700

    Add phaser necessities

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Phaser config
+const config = {
+  // Type
+  type: Phaser.AUTO,
+
+  // Scaling
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.FIT,
+  },
+
+  // Rendering options
+  render: {
+    pixelArt: true
+  },
+
+  // Proportions
+  width: 1300,
+	height: 643,
+
+  // Color of sky
+  backgroundColor: 0xd0f4f7,
+
+  // Physics
+  physics: {
+    // Default
+    default: "arcade",
+
+    // Arcade
+    arcade: {
+      // Gravity
+      gravity: {
+        y: 1500
+      },
+
+      // Options
+      enableBody: true,
+      // debug: true
+    }
+  },
+
+  // Scenes
+  // scene: {
+  //   preload,
+  //   create,
+  //   update
+  // }
+};
+
+// Phaser game
+const phaserGame = new Phaser.Game(config);
