e9a9ff58e0142d69a99b3471bca1b79597d9dc17
thcheetah777 <thcheetah777@gmail.com>
2021-11-06 15:24:06 -0700

    Load player sprite images

--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,7 @@
 // Load all the assets
 function preload() {
-
+  // Load player sprite
+  this.load.image("spy0", "assets/imgs/spy0.png");
+  this.load.image("spy1", "assets/imgs/spy1.png");
+  this.load.image("spy2", "assets/imgs/spy2.png");
 }
