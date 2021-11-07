c66a106811c216a11e119d3142997074a7ae8bef
thcheetah777 <thcheetah777@gmail.com>
2021-11-07 09:43:50 -0800

    Add player movement

--- js/create.js
+++ js/create.js
@@ -1,5 +1,11 @@
 // Create animations, sprites, and colliders
 function create() {
+  // Keyboard input
+  game.cursors = this.input.keyboard.createCursorKeys();
+
   // Create player sprite
-  game.spy = this.physics.add.sprite(500, 0, "spy0");
+  game.spy = this.physics.add.sprite(500, 500, "spy0").setScale(3).setSize(25, 24).setOffset(21, 20);
+
+  // Player bounds
+  game.spy.setCollideWorldBounds(true);
 }
--- js/update.js
+++ js/update.js
@@ -1,4 +1,48 @@
 // Update animations and sprite movement
 function update() {
+  // Controls
+  // Right
+  if (game.cursors.right.isDown) {
+    // Move right
+    game.spy.setVelocityX(350);
 
+    // Flip image
+    game.spy.flipX = false;
+
+    // Dir var
+    game.spy.dir = "R";
+
+    // Left
+  } else if (game.cursors.left.isDown) {
+    // Move left
+    game.spy.setVelocityX(-350);
+    // Flip image
+    game.spy.flipX = true;
+
+    // Dir var
+    game.spy.dir = "L";
+
+    // None
+  } else {
+    // Don't move
+    game.spy.setVelocityX(0);
+
+    // Stop anims
+    game.spy.setTexture("spy0");
+  }
+
+  // Key function
+  const keyPress = (key) => {
+    if (Phaser.Input.Keyboard.JustDown(this.input.keyboard.addKey(key))) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Jump
+  if (game.cursors.up.isDown && game.spy.body.blocked.down) {
+    // Jump
+    game.spy.setVelocityY(-1000);
+  }
 }
