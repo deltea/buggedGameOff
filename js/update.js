ef07e360adf51a891eb4971dd6c371c89943b5d6
thcheetah777 <thcheetah777@gmail.com>
2021-11-12 15:11:53 -0800

    Flip bug when shot

--- js/update.js
+++ js/update.js
@@ -52,9 +52,11 @@ function update() {
   // Shoot bug
   if (keyPress(Phaser.Input.Keyboard.KeyCodes.C)) {
     if (game.spy.flipX === false) {
-      game.bugs.create(game.spy.x + 50, game.spy.y, "bug").setScale(3).setSize(7, 5).setOffset(23, 30).setVelocityX(300).setVelocityY(-300);
+      let bug = game.bugs.create(game.spy.x + 50, game.spy.y, "bug").setScale(3).setSize(7, 5).setOffset(23, 30).setVelocityX(300).setVelocityY(-300);
+      bug.flipX = false;
     } else {
-      game.bugs.create(game.spy.x - 30, game.spy.y, "bug").setScale(3).setSize(7, 5).setOffset(23, 30).setVelocityX(-300).setVelocityY(-300);
+      let bug = game.bugs.create(game.spy.x - 30, game.spy.y, "bug").setScale(3).setSize(7, 5).setOffset(23, 30).setVelocityX(-300).setVelocityY(-300);
+      bug.flipX = true;
     }
   }
 }
