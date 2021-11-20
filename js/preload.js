6cb17fa34f7b0d00ec8f3c6995f16596aed8ca15
thcheetah777 <thcheetah777@gmail.com>
2021-11-19 17:27:51 -0800

    Fix bug

--- js/preload.js
+++ js/preload.js
@@ -4,7 +4,7 @@ function preload() {
   this.load.image("spy0", "assets/imgs/spy0.png");
   this.load.image("spy1", "assets/imgs/spy1.png");
   this.load.image("spy2", "assets/imgs/spy2.png");
-  
+
   // Load door sprite
   this.load.image("door", "assets/imgs/door.png");
 
