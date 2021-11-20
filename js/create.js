35cba79b3ca84a1e576fcfbf4554ad6103f3006d
thcheetah777 <thcheetah777@gmail.com>
2021-11-19 17:34:00 -0800

    Reorder sprites

--- js/create.js
+++ js/create.js
@@ -14,20 +14,20 @@ function create() {
   // Player bounds
   game.spy.setCollideWorldBounds(true);
 
-  // Blocks
-  game.blocks = this.physics.add.staticGroup();
-
-  // Create blocks
-  for (var x = 0; x < world.blocks.length; x++) {
-    game.blocks.create(world.blocks[x][0] * 58, world.blocks[x][1], "block").setScale(2).setOffset(-1, 0).setSize(58, 58);
-  }
-
   // Doors
   game.doors = this.physics.add.staticGroup();
+
+  // Blocks
+  game.blocks = this.physics.add.staticGroup();
 
   // Create doors
   for (var x = 0; x < world.doors.length; x++) {
     game.doors.create(world.doors[x][0], world.doors[x][1], "door").setScale(2).setSize(5, 115).setOffset(29, -25);
+  }
+
+  // Create blocks
+  for (var x = 0; x < world.blocks.length; x++) {
+    game.blocks.create(world.blocks[x][0] * 58, world.blocks[x][1], "block").setScale(2).setOffset(-1, 0).setSize(58, 58);
   }
 
   // Colliders
